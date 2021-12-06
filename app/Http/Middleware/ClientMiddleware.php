fa92ace2fee9faf3adff0501df92c8f3fde317e8
Murillo Lima <murillo_smit@hotmail.com>
2021-12-06 11:18:47 -0300

    Validacao middlware client

--- app/Http/Middleware/ClientMiddleware.php
+++ app/Http/Middleware/ClientMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class ClientMiddleware
 {
@@ -15,6 +16,10 @@ class ClientMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (Auth::user()->role != 2) {
+            return redirect()->route('dashboard.index');
+        } else {
+            return $next($request);
+        }
     }
 }
